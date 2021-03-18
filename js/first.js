6cde38c177292af598286a7b419e8a8660c32125
Lorenzo <lorenzo.antonelli89@gmail.com>
2021-03-18 13:36:03 +0100

    esercizio finito

---
+++ js/first.js
@@ -0,0 +1,30 @@
+// ho trovato 2 soluzioni le ho usate entrambi per provare funzionano entrambi
+
+$(".fa-bars").click(function(){
+
+  $(".hamburger-menu").show();
+});
+
+$(".fa-times").click(function(){
+
+  $(".hamburger-menu").hide();
+});
+
+
+// function init() {
+//
+//   $(".fa-bars").on("click",function(){
+//
+//     $(".hamburger-menu").css("display","block")
+//
+// });
+//
+//   $(".fa-times").on("click",function(){
+//
+//     $(".hamburger-menu").css("display","none")
+//
+// });
+//
+// }
+//
+// init();
